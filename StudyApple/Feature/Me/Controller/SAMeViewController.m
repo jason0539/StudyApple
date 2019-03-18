19072d86292105e099571dd06545e015075fea59
jason0539 <jason0539@163.com>
2019-03-18 16:11:48 +0800

    更多控件用法

--- StudyApple/Feature/Me/Controller/SAMeViewController.m
+++ StudyApple/Feature/Me/Controller/SAMeViewController.m
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
 
 @end
 
@@ -43,5 +46,52 @@
     int progress = (int)lroundf(sender.value);
     self.sliderLabel.text = [NSString stringWithFormat:@"%d",progress];
 }
+
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting = sender.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+    if(sender.selectedSegmentIndex == 0){
+        self.leftSwitch.hidden = NO;
+        self.rightSwitch.hidden = NO;
+        self.doSomethingButton.hidden = YES;
+    }else{
+        self.leftSwitch.hidden = YES;
+        self.rightSwitch.hidden = YES;
+        self.doSomethingButton.hidden = NO;
+    }
+}
+- (IBAction)buttonPressed:(UIButton *)sender {
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Are you Sure" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Yes,I'm Sure~" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        NSString *msg;
+        if ([self.nameField.text length]>0) {
+            msg = [NSString stringWithFormat:@"You can breathe easy,%@ ,everything went ok.",self.nameField.text];
+        }else{
+            msg = @"You can breath easy,everything went Ok.";
+        }
+        UIAlertController *controller2 = [UIAlertController alertControllerWithTitle:@"Something Was Done" message:msg preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *cancleAction = [UIAlertAction actionWithTitle:@"Phew!" style:UIAlertActionStyleCancel handler:nil];
+        [controller2 addAction:cancleAction];
+        [self presentViewController:controller2 animated:YES completion:nil];
+    }];
+    
+    UIAlertAction *noAction = [UIAlertAction actionWithTitle:@"No Way" style:UIAlertActionStyleCancel handler:nil];
+    
+    [controller addAction:yesAction];
+    [controller addAction:noAction];
+    
+    UIPopoverPresentationController *ppc = controller.popoverPresentationController;
+    if (ppc != nil) {
+        ppc.sourceView = sender;
+        ppc.sourceRect = sender.bounds;
+    }
+    [self presentViewController:controller animated:yesAction completion:nil];
+}
+
 
 @end
