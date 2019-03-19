a24e4987e4f10e4398ca0aa59cf53879374dd95f
jason0539 <jason0539@163.com>
2019-03-19 16:33:05 +0800

    双列picker view

--- StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.m
+++ StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.m
@@ -8,9 +8,13 @@
 
 #import "SATabBarOneViewController.h"
 
+#define kFillingComponent 0
+#define kBreadComponent   1
+
 @interface SATabBarOneViewController ()
 @property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
-@property (strong,nonatomic) NSArray *characterName;
+@property (strong, nonatomic) NSArray *fillingTypes;
+@property (strong, nonatomic) NSArray *breadTypes;
 
 @end
 
@@ -19,15 +23,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.characterName = @[@"Luke", @"Leia", @"Han", @"Chewbacca",
-                            @"Artoo", @"Threepio", @"Lando"];
+    self.fillingTypes = @[@"Ham", @"Turkey", @"Peanut Butter", @"Tuna Salad",
+                          @"Chicken Salad", @"Roast Beef", @"Vegemite"];
+    self.breadTypes = @[@"White", @"Whole Wheat", @"Rye", @"Sourdough",
+                        @"Seven Grain"];
 }
 - (IBAction)buttonPressed:(UIButton *)sender {
-    NSInteger row = [self.pickerView selectedRowInComponent:0];
-    NSString *selected = self.characterName[row];
-    NSString *title = [[NSString alloc]initWithFormat:@"You selected %@",selected];
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choosed" message:title preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
+    NSInteger fillingRow = [self.pickerView selectedRowInComponent:
+                            kFillingComponent];
+    NSInteger breadRow = [self.pickerView selectedRowInComponent:
+                    kBreadComponent];
+    
+    
+    NSString *filling = self.fillingTypes[fillingRow];
+    NSString *bread = self.breadTypes[breadRow];
+    
+    NSString *message = [[NSString alloc] initWithFormat:
+                         @"Your %@ on %@ bread will be right up.", filling, bread];
+    
+    UIAlertController *alert =
+    [UIAlertController alertControllerWithTitle:@"Thank you for your order"
+                                        message:message
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Great!"
+                                                     style:UIAlertActionStyleDefault handler:nil];
     [alert addAction:action];
     [self presentViewController:alert animated:YES completion:nil];
 }
@@ -45,16 +64,24 @@
 #pragma mark -
 #pragma mark Picker Data Source Methods
 - (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    return [self.characterName count];
+    if (component == kBreadComponent) {
+        return self.breadTypes.count;
+    }else{
+        return self.fillingTypes.count;
+    }
 }
 
 #pragma mark Picker Delegate Methods
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return self.characterName[row];
+    if (component == kBreadComponent) {
+        return self.breadTypes[row];
+    }else{
+        return self.fillingTypes[row];
+    }
 }
 
 @end
