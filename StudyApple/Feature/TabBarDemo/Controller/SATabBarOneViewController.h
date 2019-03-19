102497934629f175bc1d74df08530f7264158d7f
jason0539 <jason0539@163.com>
2019-03-19 16:20:16 +0800

    picker view用法

--- StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.h
+++ StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SATabBarOneViewController : UIViewController
+@interface SATabBarOneViewController : UIViewController <UIPickerViewDelegate,UIPickerViewDataSource>
 
 @end
 
--- StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.m
+++ StudyApple/Feature/TabBarDemo/Controller/SATabBarOneViewController.m
@@ -9,6 +9,8 @@
 #import "SATabBarOneViewController.h"
 
 @interface SATabBarOneViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (strong,nonatomic) NSArray *characterName;
 
 @end
 
@@ -17,6 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.characterName = @[@"Luke", @"Leia", @"Han", @"Chewbacca",
+                            @"Artoo", @"Threepio", @"Lando"];
+}
+- (IBAction)buttonPressed:(UIButton *)sender {
+    NSInteger row = [self.pickerView selectedRowInComponent:0];
+    NSString *selected = self.characterName[row];
+    NSString *title = [[NSString alloc]initWithFormat:@"You selected %@",selected];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choosed" message:title preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 /*
@@ -28,5 +41,20 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+- (NSInteger)numberOfComponentsInPickerView:(nonnull UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(nonnull UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [self.characterName count];
+}
+
+#pragma mark Picker Delegate Methods
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return self.characterName[row];
+}
 
 @end
