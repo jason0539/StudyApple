de133cfc7622472c39ec45f78646d49f22df17f3
jason0539 <jason0539@163.com>
2019-03-19 17:25:37 +0800

    picker view更复杂用法，声音图片自定义

--- StudyApple/Feature/TabBarDemo/Controller/SATabBarTwoViewController.h
+++ StudyApple/Feature/TabBarDemo/Controller/SATabBarTwoViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SATabBarTwoViewController : UIViewController
+@interface SATabBarTwoViewController : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate>
 
 @end
 
